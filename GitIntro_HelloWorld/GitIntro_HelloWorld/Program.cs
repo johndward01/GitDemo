be74d044f60f84e88288f8892866f370ac411623
johndward01 <johndward01@gmail.com>
2022-04-19 08:48:00 -0500

    init

---
+++ GitIntro_HelloWorld/GitIntro_HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GitIntro_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Class!");
+            PrintHello();
+        }
+
+        public static void PrintHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
